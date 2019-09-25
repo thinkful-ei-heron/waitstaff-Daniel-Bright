6eb5a1690cd7ef7fe7330d055c597a4dede09f1b
Daniel Bright <brahyt@gmail.com>
2019-09-25 14:14:26 -0700

    fixed validation

--- app.js
+++ app.js
@@ -1,108 +1,115 @@
+'use strict'
 function earningCalc(){
-  let totalTip = 0
+  let totalTip = 0;
   STORE.meals.forEach(e => {
-    totalTip += e.basePrice * (e.tipPercent/100)
-  })
-  return totalTip.toFixed(2)
+    totalTip += e.basePrice * (e.tipPercent/100);
+  });
+  return totalTip.toFixed(2);
 }
+
 function earningsSnip(){
-  let tipTotal = earningCalc()
-  let ammountOfMeals = STORE.numMeal
-  let avgTip = tipTotal / ammountOfMeals
+  let tipTotal = earningCalc();
+  let ammountOfMeals = STORE.numMeal;
+  let avgTip = tipTotal / ammountOfMeals;
   return `
     <ul>
       <li>Tip Total: ${tipTotal}</li>
       <li>Number of Meals: ${ammountOfMeals}</li>
       <li>Avg Tip Per Meal: ${avgTip.toFixed(2)}</li>
     </ul>
-  `
-}
-
-function increaseMealsNum(){
-  STORE.numMeal += 1;
+  `;
 }
 
 function renderEarningsInfo(){
   $('.js-earn-info').html(earningsSnip());
 }
 
+function increaseMealsNum(){
+  STORE.numMeal += 1;
+}
+
 function customerChangesSnip(basePrice, taxRate, tipPercent){
-  let subtotal = basePrice * (1 + (taxRate/100))
-  let tip = subtotal * (tipPercent/100)
-  let total = subtotal + tip
+  let subtotal = basePrice * (1 + (taxRate/100));
+  let tip = subtotal * (tipPercent/100);
+  let total = subtotal + tip;
   return `
   <ul>
     <li>Sub-total: ${subtotal.toFixed(2)}</li>
     <li>Tip: ${tip.toFixed(2)}</li>
     <li>Total: ${total.toFixed(2)}</li>
   </ul>
-  `
+  `;
 }
 
 function renderCustomerChanges(basePrice, taxRate, tipPercent){
-  $('.js-cus-changes').html(customerChangesSnip(basePrice, taxRate, tipPercent))
+  $('.js-cus-changes').html(customerChangesSnip(basePrice, taxRate, tipPercent));
 }
 
 function createObj(basePrice, taxRate, tipPercent){
-  return {'basePrice': basePrice, 'taxRate': taxRate, 'tipPercent': tipPercent}
+  return {'basePrice': basePrice, 'taxRate': taxRate, 'tipPercent': tipPercent};
+}
+
+function checkValid(){
+  let basePrice = parseFloat($('#base-meal-price').val());
+  let tax = parseFloat($('#tax-rate').val());
+  let tip = parseFloat($('#tip-percent').val());
+  return !(isNaN(basePrice) && isNaN(tax) && isNaN(tip))
 }
 
 function resetForm(){
-  $('#base-meal-price').val('')
-  $('#tax-rate').val('')
-  $('#tip-percent').val('')
+  $('#base-meal-price').val(null);
+  $('#tax-rate').val(null);
+  $('#tip-percent').val(null);
 }
 
 function mealDetailsToStore(){
-  let basePrice = $('#base-meal-price').val()
-  let taxRate = $('#tax-rate').val()
-  let tipPercent = $('#tip-percent').val()
-  STORE.meals.unshift(createObj(basePrice, taxRate, tipPercent))
-  increaseMealsNum()
-  renderCustomerChanges(basePrice, taxRate, tipPercent)
+  let basePrice = parseFloat($('#base-meal-price').val());
+  let taxRate = parseFloat($('#tax-rate').val());
+  let tipPercent = parseFloat($('#tip-percent').val());
+  STORE.meals.unshift(createObj(basePrice, taxRate, tipPercent));
+  increaseMealsNum();
+  renderCustomerChanges(basePrice, taxRate, tipPercent);
 }
 
-//function checkInput(){
-//  $('input').each(function() {
-//    if(typeof this.value !== /[0-9]/gm) {
-//      return false
-//    }else{
-//      return true
-//    }
-//  })
-//}
-
-function buttonSubmitWatch(){
+function buttonSubmitListener(){
   $('.js-data-field').on('click', '#submit', event => {
-    event.preventDefault()
-      mealDetailsToStore()
-      renderEarningsInfo()
-      resetForm()
-  })
+    event.preventDefault();
+    console.log(checkValid())
+    if (checkValid()){
+      mealDetailsToStore();
+      renderEarningsInfo();
+      resetForm();
+    }
+    else {
+      resetForm();
+      alert('must use numbers')
+    }
+    
+  });
 }
 
 function resetButton(){
   $('.js-reset').on('click', function(){
-    STORE.meals.length = 0
-    STORE.numMeal = 0
-    renderEarningsInfo()
-    resetForm()
-    $('.js-cus-changes').html(customerChangesSnip(0, 0, 0))
-  })
+    STORE.meals.length = 0;
+    STORE.numMeal = 0;
+    renderEarningsInfo();
+    resetForm();
+    $('.js-cus-changes').html(customerChangesSnip(0, 0, 0));
+  });
 }
 
-function cancelButton(){
+function cancelButtonListener(){
   $('#js-cancel').on('click', event => {
-    event.preventDefault()
-    resetForm()
-  })
+    event.preventDefault();
+    resetForm();
+  });
 }
 
 function start(){
-  buttonSubmitWatch()
-  earningCalc()
-  resetButton()
-  cancelButton()
+  buttonSubmitListener();
+  cancelButtonListener();
+  earningCalc();
+  resetButton();
 }
 
-$(start)
+$(start);
